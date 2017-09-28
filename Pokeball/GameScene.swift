d39f2d6359bbc15b7bdad8536b65e5ee98550ded
Denis G <denisglb@yahoo.co.uk>
2017-09-28 13:23:20 +0100

    added refactoring

--- Pokeball/GameScene.swift
+++ Pokeball/GameScene.swift
@@ -34,94 +34,82 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         
-    pokeball = SKSpriteNode(imageNamed: "pokeball")
-    pokeball?.physicsBody?.categoryBitMask = pokeballCategory
-    pokeball?.physicsBody?.contactTestBitMask = monCategory
-    pokeball.position = CGPoint(x: self.frame.size.width/2, y: self.size.height/2)
-    self.addChild(pokeball)
-    
-    pokeball.physicsBody = SKPhysicsBody(texture: pokeball.texture!,
-                                           size: pokeball.texture!.size())
-    pokeball.physicsBody?.usesPreciseCollisionDetection = true
-    
-    scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
-        func createMon() {
-            let mon = SKSpriteNode(imageNamed: "pikachu")
-            mon.physicsBody = SKPhysicsBody(rectangleOf: mon.size)
-            mon.physicsBody?.categoryBitMask = monCategory
-            mon.physicsBody?.contactTestBitMask = pokeballCategory
-            addChild(mon)
-
-            let height = frame.height
-            let width = frame.width
-            
-            let randomPosition = CGPoint(x:CGFloat(arc4random()).truncatingRemainder(dividingBy: width),
-                                         y: CGFloat(arc4random()).truncatingRemainder(dividingBy: height))
-
-            mon.position = randomPosition
-        }
+        pokeball = SKSpriteNode(imageNamed: "pokeball")
+        pokeball?.physicsBody?.categoryBitMask = pokeballCategory
+        pokeball?.physicsBody?.contactTestBitMask = monCategory
+        pokeball.position = CGPoint(x: self.frame.size.width/2, y: self.size.height/2)
+        self.addChild(pokeball)
         
-        func createRocket() {
-            let rocket = SKSpriteNode(imageNamed: "rocket")
-            rocket.physicsBody = SKPhysicsBody(rectangleOf: rocket.size)
-            rocket.physicsBody?.categoryBitMask = rocketCategory
-            rocket.physicsBody?.contactTestBitMask = pokeballCategory
-            addChild(rocket)
-            
-            
-            let height = frame.height
-            let width = frame.width
-            
-            let randomPosition = CGPoint(x:CGFloat(arc4random()).truncatingRemainder(dividingBy: width),
-                                         y: CGFloat(arc4random()).truncatingRemainder(dividingBy: height))
-            
-            
-            rocket.position = randomPosition
-            
-            
-        }
-    
-    self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-    self.physicsWorld.contactDelegate = self
-    
-    level = SKLabelNode(text: "Level: Makers4Lyf")
-    level.position = CGPoint(x:200, y: self.frame.size.height - 60)
-    level.fontName = "AmericanTypewriter-Bold"
-    level.fontSize = 36
-    level.fontColor = UIColor.white
-    
-    self.addChild(level)
-        rocketTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: { (timer) in
-            createRocket()
-        })
+        pokeball.physicsBody = SKPhysicsBody(texture: pokeball.texture!,
+                                               size: pokeball.texture!.size())
+        pokeball.physicsBody?.usesPreciseCollisionDetection = true
         
-        monTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
-           createMon()
-        })
+        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
 
         
-    motionManager.accelerometerUpdateInterval = 0.2
-    motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
-        if let accelerometerData = data {
-            let acceleration = accelerometerData.acceleration
-            self.xAcceleration = CGFloat(acceleration.x) * 0.50 + self.xAcceleration * 0.50
-            self.yAcceleration = CGFloat(acceleration.y) * 0.50 + self.yAcceleration * 0.50
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        level = SKLabelNode(text: "Level: Makers4Lyf")
+        level.position = CGPoint(x:200, y: self.frame.size.height - 60)
+        level.fontName = "AmericanTypewriter-Bold"
+        level.fontSize = 36
+        level.fontColor = UIColor.white
+        
+        self.addChild(level)
+            rocketTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: { (timer) in
+                self.createRocket()
+            })
+        
+            monTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+               self.createMon()
+            })
+        
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
+            if let accelerometerData = data {
+                let acceleration = accelerometerData.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.50 + self.xAcceleration * 0.50
+                self.yAcceleration = CGFloat(acceleration.y) * 0.50 + self.yAcceleration * 0.50
+            }
         }
     }
+    func createMon() {
+        let mon = SKSpriteNode(imageNamed: "pikachu")
+        mon.physicsBody = SKPhysicsBody(rectangleOf: mon.size)
+        mon.physicsBody?.categoryBitMask = monCategory
+        mon.physicsBody?.contactTestBitMask = pokeballCategory
+        addChild(mon)
+        
+        let height = frame.height
+        let width = frame.width
+        
+        let randomPosition = CGPoint(x:CGFloat(arc4random()).truncatingRemainder(dividingBy: width),
+                                     y: CGFloat(arc4random()).truncatingRemainder(dividingBy: height))
+        
+        mon.position = randomPosition
+    }
     
-}
-
+    func createRocket() {
+        let rocket = SKSpriteNode(imageNamed: "rocket")
+        rocket.physicsBody = SKPhysicsBody(rectangleOf: rocket.size)
+        rocket.physicsBody?.categoryBitMask = rocketCategory
+        rocket.physicsBody?.contactTestBitMask = pokeballCategory
+        addChild(rocket)
+        
+        let height = frame.height
+        let width = frame.width
+        
+        let randomPosition = CGPoint(x:CGFloat(arc4random()).truncatingRemainder(dividingBy: width),
+                                     y: CGFloat(arc4random()).truncatingRemainder(dividingBy: height))
+        
+        rocket.position = randomPosition
+    }
+    
     
     override func didSimulatePhysics() {
         pokeball.position.x += xAcceleration * 35
         pokeball.position.y += yAcceleration * 35
-        
-        if pokeball.position.x < -20 {
-            pokeball.position = CGPoint(x: pokeball.position.x, y: pokeball.position.y)
-        }else if pokeball.position.x > self.size.width + 20 {
-            pokeball.position = CGPoint(x: pokeball.position.x, y: pokeball.position.y)
-        }
-        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -161,7 +149,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(yourScore)
 
     }
-    
 
 }
 
